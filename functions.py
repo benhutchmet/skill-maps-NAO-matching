74551db12961881b3bb375463d5fc78fd6e8aa16
benhutchmet <b.hutchins@pgr.reading.ac.uk>
2023-09-19 11:55:07 +0100

    regrid sel region function added

--- functions.py
+++ functions.py
@@ -24,5 +24,103 @@ import pdb
 import datetime
 import iris.quickplot as qplt
 
+# Import CDO
+from cdo import *
+cdo = Cdo()
+
+
+# Import the dictionaries
+import dictionaries as dic
+
+# Define a function which regrids and selects the region according to the specified gridspec
+# The same is done for the model data
+def regrid_and_select_region(variable, region, observations_path, level=None):
+    """
+    Regrids and selects the region according to the specified gridspec.
+    
+    Parameters
+    ----------
+    variable : str
+        Variable name.
+    region : str
+        Region name.
+    observations_path : str
+        Path to the observations.
+    level : str, optional
+        Level name. The default is None.
+        
+    Returns
+    -------
+    regrid_obs_path : str
+        Path to the regridded observations.
+    """
+
+    # Check whether the gridspec path exists for the specified region
+    gridspec_path = f"/home/users/benhutch/gridspec/gridspec-{region}.txt"
+
+    if not os.path.exists(gridspec_path):
+        print('The gridspec path does not exist for the specified region: ', region)
+        sys.exit()
+
+    # Form the wind speed variables list
+    wind_speed_variables = ['ua', 'va', 'var131', 'var132']
+
+    if variable in wind_speed_variables and level is None:
+        print('The level must be specified for the wind speed variables')
+        sys.exit()
+
+    # Form the regrid sel region path accordingly
+    if variable in wind_speed_variables:
+        regrid_obs_path = f"/home/users/benhutch/ERA5/{region}_regrid_sel_region_{variable}_{level}.nc"
+    else:
+        regrid_obs_path = f"/home/users/benhutch/ERA5/{region}_regrid_sel_region.nc"
+    
+    # Check whether the regrid sel region path exists
+    if not os.path.exists(regrid_obs_path):
+        print('The regrid sel region path does not exist')
+        print('Regridding and selecting the region')
+
+        # Regrid and select the region using CDO
+        cdo.remapbil(gridspec_path, input=observations_path, output=regrid_obs_path)
+
+    return regrid_obs_path
+
+
+
 # We want to write a function which reads and processes the observations
 # then returns the obs anomaly field
+def read_obs(variable, region, forecast_range, season, observations_path, level=None):
+    """
+    Processes the observations to have the same grid as the model data
+    using CDO. Then selects the region and season. Then calculates the
+    anomaly field using the climatology. Then calculates the annual 
+    mean of the anomaly field. Then selects the forecast range (e.g. 
+    years 2-5). Then selects the season. Then returns the anomaly field.
+    
+
+    Parameters
+    ----------
+    variable : str
+        Variable name.
+    region : str
+        Region name.
+    forecast_range : str
+        Forecast range.
+    season : str
+        Season name.
+    observations_path : str
+        Path to the observations.
+    level : str, optional
+        Level name. The default is None.
+
+    Returns
+    -------
+    obs_anomaly : iris.cube.Cube
+        Anomaly field.
+
+    """
+
+    # First check that the obs_path exists
+    if not os.path.exists(observations_path):
+        print('The observations path does not exist')
+        sys.exit()
