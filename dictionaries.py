0833b01e41fe313dd6f7662a0dae99c5cb856507
benhutchmet <b.hutchins@pgr.reading.ac.uk>
2023-09-20 11:30:17 +0100

    making sure that the obs and the model cover the same years

--- dictionaries.py
+++ dictionaries.py
@@ -27,3 +27,7 @@ season_month_map = {
 }
 
 obs_path = "/home/users/benhutch/ERA5/adaptor.mars.internal-1691509121.3261805-29348-4-3a487c76-fc7b-421f-b5be-7436e2eb78d7.nc"
+
+psl_full_models = [ "BCC-CSM2-MR", "MPI-ESM1-2-HR", "CanESM5", "CMCC-CM2-SR5", "HadGEM3-GC31-MM", "EC-Earth3", "MPI-ESM1-2-LR", "FGOALS-f3-L", "MIROC6", "IPSL-CM6A-LR", "CESM1-1-CAM5-CMIP5", "NorCPM1" ]
+
+base_dir_skm_pro = "/home/users/benhutch/skill-maps-processed-data"
